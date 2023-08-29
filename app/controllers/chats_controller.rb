a63dc29dc30de7b562cbdc2c55e78507fe5545ff
Kominami-Hisashi <komi73tov@gmail.com>
2023-08-30 06:29:16 +0900

    練習

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,26 @@
+class ChatsController < ApplicationController
+  before_action :set_token, only: :search
+
+  def index; end
+
+  def search
+    @client = OpenAI::Client.new(access_token: @api_key)
+    response = @client.chat(
+      parameters: {
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: @query }],
+      })
+
+    @chats = response.dig("choices", 0, "message", "content")
+  end
+
+  private
+
+  def text_params
+    @query = params[:text]
+  end
+
+  def set_token
+    @api_key = Rails.application.credentials.dig(:openai, :api_key)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: 'chats#index'
+  get 'search', to: 'chats#search'
   # Defines the root path route ("/")
   # root "articles#index"
 end
