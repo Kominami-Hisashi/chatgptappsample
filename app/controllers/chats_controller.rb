53309fe4fe7590d67232d8dc7c07c439c6816ed3
Kominami-Hisashi <komi73tov@gmail.com>
2023-09-02 23:16:12 +0900

    哲人設定テスト

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,17 +1,29 @@
 class ChatsController < ApplicationController
-  before_action :set_token, only: :search
+  before_action :set_token
 
   def index; end
 
   def search
-    @client = OpenAI::Client.new(access_token: @api_key)
-    response = @client.chat(
+    client = OpenAI::Client.new(
+      access_token: ENV['API_KEY'],
+      uri_base: "https://oai.hconeai.com/",
+      request_timeout: 240
+    )
+    text = text_params()
+    initial_message = { role: "system", content: "ou are Karl Marx. Provide insightful and constructive responses without outright negation to the queries." }
+    user_message = { role: "user", content: text }
+    response = client.chat(
       parameters: {
-          model: "gpt-3.5-turbo",
-          messages: [{ role: "user", content: @query }],
-      })
+        model: "gpt-3.5-turbo",
+        messages: [initial_message, user_message],
+        temperature: 0.7,
+      }
+    )
+
+    puts response
 
     @chats = response.dig("choices", 0, "message", "content")
+    puts @chats
   end
 
   private
