0d4797f673d1b078eb9ad7c3034f825eb7f517f0
Kominami-Hisashi <komi73tov@gmail.com>
2023-09-24 13:37:03 +0900

    回答アニメーション実装

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -3,3 +3,13 @@ window.onload = function() {
   // Add .loaded to .loading
   spinner.classList.add('loaded');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  let searchButton = document.getElementById("submit-button");
+  let loadingSpinner = document.getElementById("loading");
+
+  searchButton.addEventListener('click', function() {
+    // loadingSpinnerの中のloadedというクラスを削除
+    loadingSpinner.classList.remove('loaded');
+  });
+});
